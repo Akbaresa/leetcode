25c74340c68cf9237429de2cbde4e97be5990e37
esa <takutoakbar354@gmail.com>
2023-08-03 15:23:21 +0700

    leet code

--- Best time to buy and sell stock/src/App.java
+++ Best time to buy and sell stock/src/App.java
@@ -1,10 +1,33 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int buy = prices[0], max_profit = 0;
+        for (int i = 1; i < n; i++) {
 
-        return -1;
+            // Cek harga terendah
+            if (buy > prices[i])
+                buy = prices[i];
+
+            // cek profit tertinggi
+            else if (prices[i] - buy > max_profit)
+                max_profit = prices[i] - buy;
+        }
+        return max_profit;
     }
 
     public static void main(String[] args) {
+        Solution solution = new Solution();
 
+        int[] prices = { 2, 4, 1 };
+        int[] prices1 = { 1, 2 };
+        int[] prices2 = { 7, 1, 5, 6, 3, 4 };
+
+        int result = solution.maxProfit(prices);
+        int result1 = solution.maxProfit(prices1);
+        int result2 = solution.maxProfit(prices2);
+
+        System.out.println(result == 2);
+        System.out.println(result1 == 1);
+        System.out.println(result2 == 5);
     }
 }
